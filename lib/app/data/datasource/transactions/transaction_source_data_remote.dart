4570cbcf34a76c7b137cc227408c4eb24854f0ab
Diyanto TI <diyanto2911@gmail.com>
2021-11-20 17:11:54 +0700

    fix transaksiion

--- lib/app/data/datasource/transactions/transaction_source_data_remote.dart
+++ lib/app/data/datasource/transactions/transaction_source_data_remote.dart
@@ -41,31 +41,37 @@ class TransactionRemoteDataSource extends BaseDio {
         double afterDiscount = 0;
         if (item.discount != null) {
           if (item.discount!.discountType == "percent") {
-            if (item.discount!.discountMaxPriceOff == "0" || item.discount!.discountMaxPriceOff == null) {
-              discount = item.subTotal! * (int.parse(item.discount!.discountPercent!) / 100);
+            if (item.discount!.discountMaxPriceOff == "0" ||
+                item.discount!.discountMaxPriceOff == null) {
+              discount = item.subTotal! *
+                  (int.parse(item.discount!.discountPercent!) / 100);
               afterDiscount = item.subTotal! - discount;
             } else {
-               discount = item.subTotal! * (int.parse(item.discount!.discountPercent!) / 100);
-              if (discount >= double.parse(item.discount!.discountMaxPriceOff!)) {
-                discount=double.parse(item.discount!.discountMaxPriceOff!);
-                afterDiscount = (item.subTotal! - double.parse(item.discount!.discountMaxPriceOff!));
+              discount = item.subTotal! *
+                  (int.parse(item.discount!.discountPercent!) / 100);
+              if (discount >=
+                  double.parse(item.discount!.discountMaxPriceOff!)) {
+                discount = double.parse(item.discount!.discountMaxPriceOff!);
+                afterDiscount = (item.subTotal! -
+                    double.parse(item.discount!.discountMaxPriceOff!));
               } else {
                 afterDiscount = item.subTotal! - discount;
               }
             }
-          }else{
-            discount=double.tryParse(item.discount!.discountMaxPriceOff!)!;
-            afterDiscount= afterDiscount = item.subTotal! - discount;
+          } else {
+            discount = double.tryParse(item.discount!.discountMaxPriceOff!)!;
+            afterDiscount = afterDiscount = item.subTotal! - discount;
           }
         }
 
-          // print(item.discount!.discountMaxPriceOff!);
+        // print(item.discount!.discountMaxPriceOff!);
         listProduct.add({
           "product_id": "${item.dataProduct!.productId}",
           "product_qty": item.qty,
           "product_price": item.dataProduct!.productPrice,
           "total_weight": item.dataProduct!.productWeight,
-          "discount_code": item.discount == null ? null : item.discount!.discountId,
+          "discount_code":
+              item.discount == null ? null : item.discount!.discountId,
           "sub_total": item.subTotal,
           "discount": item.discount == null
               ? null
@@ -75,7 +81,6 @@ class TransactionRemoteDataSource extends BaseDio {
           "price_after_discount": afterDiscount
         });
       });
-
 
       Map<dynamic, dynamic> data = {
         if (auth.roleName == "Pemilik Toko") "user_id": "${auth.userId}",
@@ -98,34 +103,66 @@ class TransactionRemoteDataSource extends BaseDio {
         "products": listProduct
       };
       print(jsonEncode(data));
-      response = await dio.post(MyString.storeTransaction, data: jsonEncode(data), options: options);
-      return ResponseMessage(message: response!.data['message'], status: response!.data['status'], data: response!.data['data']);
+      response = await dio.post(MyString.storeTransaction,
+          data: jsonEncode(data), options: options);
+      return ResponseMessage(
+          message: response!.data['message'],
+          status: response!.data['status'],
+          data: response!.data['data']);
     } on DioError catch (e) {
       print(e);
       throw ServerException();
     }
   }
 
-  Future<TransactionModel> getTransaction({bool userID = false, String? statusTransaction, String? statusPayment}) async {
+  Future<TransactionModel> getTransaction(
+      {bool userID = false,
+      String? statusTransaction,
+      String? statusPayment}) async {
     try {
       response = await dio.get("${MyString.getTransaction}",
-          queryParameters: {"store_id": "${auth.storeId}", if (auth.roleName.toString() != "Pemilik Toko") "employe_id": "${auth.userId}", if (statusTransaction != null) "transaction_status": "$statusTransaction", if (statusPayment != null) "transaction_payment_status": "${statusPayment}"},
+          queryParameters: {
+            "store_id": "${auth.storeId}",
+            if (auth.roleName.toString() != "Pemilik Toko")
+              "employe_id": "${auth.userId}",
+            if (statusTransaction != null)
+              "transaction_status": "$statusTransaction",
+            if (statusPayment != null)
+              "transaction_payment_status": "${statusPayment}"
+          },
           options: options);
-      await APICacheManager().addCacheData(APICacheDBModel(key: 'API_TRANSACTION_MODEL', syncData: jsonEncode(response!.data)));
+      await APICacheManager().addCacheData(APICacheDBModel(
+          key: 'API_TRANSACTION_MODEL', syncData: jsonEncode(response!.data)));
       return TransactionModel.fromJson(response!.data);
     } on DioError catch (e) {
       print(e);
-      var cacheData = await APICacheManager().getCacheData('API_TRANSACTION_MODEL');
+      var cacheData =
+          await APICacheManager().getCacheData('API_TRANSACTION_MODEL');
       var result = TransactionModel.fromJson(jsonDecode(cacheData.syncData));
       return result;
     }
   }
 
-  Future<ResponseMessage> changeStatusTransaction({String? paymentMethodID, String? paymentMethodStatus, String? transactionPaymentDate, String? transactionStatus, double? transactionPay, String? transactionID, double? transactionReceived, List<CartModel>? listCart}) async {
+  Future<ResponseMessage> changeStatusTransaction(
+      {String? paymentMethodID,
+      String? paymentMethodStatus,
+      String? transactionPaymentDate,
+      String? transactionStatus,
+      double? transactionPay,
+      String? transactionID,
+      String? transactionReasonCancel,
+      double? transactionReceived,
+      List<CartModel>? listCart}) async {
     try {
       Map<dynamic, dynamic> data = {
-        if (auth.roleName == "Pemilik Toko") "user_id": "${auth.userId}" else "user_id": null,
-        if (auth.roleName == "Pemilik Toko") "employe_id": null else "user_id": "${auth.userId}",
+        if (auth.roleName == "Pemilik Toko")
+          "user_id": "${auth.userId}"
+        else
+          "user_id": null,
+        if (auth.roleName == "Pemilik Toko")
+          "employe_id": null
+        else
+          "user_id": "${auth.userId}",
         "store_id": "${auth.storeId}",
         "transaction_payment_method_id": paymentMethodID,
         "transaction_payment_status": paymentMethodStatus,
@@ -134,44 +171,59 @@ class TransactionRemoteDataSource extends BaseDio {
         "transaction_id": transactionID,
         "transaction_pay": transactionPay,
         "transaction_received": transactionReceived,
+        "transaction_reason_cancel": transactionReasonCancel,
       };
       print(jsonEncode(data));
-      response = await dio.post(MyString.changeStatusTransaction, data: jsonEncode(data), options: options);
-      return ResponseMessage(message: response!.data['message'], status: response!.data['status'], data: response!.data['data']);
+      response = await dio.post(MyString.changeStatusTransaction,
+          data: jsonEncode(data), options: options);
+      return ResponseMessage(
+          message: response!.data['message'],
+          status: response!.data['status'],
+          data: response!.data['data']);
     } on DioError catch (e) {
       print(e);
       throw ServerException();
     }
   }
 
-  Future<ReportTransaction> getReportTransaction({String? startDate, String? dueDate, String type = 'monthly',List<dynamic>? listStore}) async {
+  Future<ReportTransaction> getReportTransaction(
+      {String? startDate,
+      String? dueDate,
+      String type = 'monthly',
+      List<dynamic>? listStore}) async {
     try {
-
       Map<dynamic, dynamic> data = {
         "user_id": "${auth.userIdOwner}",
         // if (type == "custom") "type_report": "custom",
-        if (listStore!=null) "type_report": "custom",
-        if (listStore!=null) "store": listStore,
-        if (listStore==null) "type_report": "all",
-         "start_date": startDate,
-         "due_date": dueDate,
+        if (listStore != null) "type_report": "custom",
+        if (listStore != null) "store": listStore,
+        if (listStore == null) "type_report": "all",
+        "start_date": startDate,
+        "due_date": dueDate,
         // if (type != "custom") "type_report": type,
       };
-      response = await dio.post("${MyString.getReportTransaction}", data: jsonEncode(data), options: options);
+      response = await dio.post("${MyString.getReportTransaction}",
+          data: jsonEncode(data), options: options);
       if (type != "custom")
-        await APICacheManager().addCacheData(APICacheDBModel(key: 'API_REPORT_TRANSACTION_MONTHLY_MODEL', syncData: jsonEncode(response!.data)));
+        await APICacheManager().addCacheData(APICacheDBModel(
+            key: 'API_REPORT_TRANSACTION_MONTHLY_MODEL',
+            syncData: jsonEncode(response!.data)));
       else
-        await APICacheManager().addCacheData(APICacheDBModel(key: 'API_REPORT_TRANSACTION_MODEL', syncData: jsonEncode(response!.data)));
+        await APICacheManager().addCacheData(APICacheDBModel(
+            key: 'API_REPORT_TRANSACTION_MODEL',
+            syncData: jsonEncode(response!.data)));
       return ReportTransaction.fromJson(response!.data);
     } on DioError catch (e) {
       print(e);
       var cacheData;
-      if (type != "custom")
-        if(await APICacheManager().isAPICacheKeyExist("API_REPORT_TRANSACTION_MONTHLY_MODEL"))
-        cacheData = await APICacheManager().getCacheData('API_REPORT_TRANSACTION_MONTHLY_MODEL');
-      else
-        if(await APICacheManager().isAPICacheKeyExist("API_REPORT_TRANSACTION_MODEL"))
-        cacheData = await APICacheManager().getCacheData('API_REPORT_TRANSACTION_MODEL');
+      if (type != "custom") if (await APICacheManager()
+          .isAPICacheKeyExist("API_REPORT_TRANSACTION_MONTHLY_MODEL"))
+        cacheData = await APICacheManager()
+            .getCacheData('API_REPORT_TRANSACTION_MONTHLY_MODEL');
+      else if (await APICacheManager()
+          .isAPICacheKeyExist("API_REPORT_TRANSACTION_MODEL"))
+        cacheData = await APICacheManager()
+            .getCacheData('API_REPORT_TRANSACTION_MODEL');
       var result = ReportTransaction.fromJson(jsonDecode(cacheData.syncData));
       return result;
     }
